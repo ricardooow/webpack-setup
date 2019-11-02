d09143c9fc1b145bb2c063f853b2b810f8cb4ebf
ricardooow <boshuisen1@hotmail.com>
2019-11-02 16:11:56 +0100

    relocated config from root to ./config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, '../build'),
         publicPath: '/',
         filename: 'bundle.js'
     },
