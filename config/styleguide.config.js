238338d094bbce1d00e2ea72d92e017edc5899f0
ricardooow <boshuisen1@hotmail.com>
2019-11-16 20:47:08 +0100

    initial styleguidist setup

---
+++ config/styleguide.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    components: '../src/components/**/[A-Z]*.js',
+    webpackConfig: require('./webpack.config.js')
+}
---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { buttonStyles } from "Button.scss";
+
+
+export const ButtonExample = () => {
+    return (
+        <>
+            <button className={buttonStyles.btnPrimary}>Primary button</button>
+        </>
+    )
+}
